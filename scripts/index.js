03501f2f859206f341a646c08f4f9189d8548cca
sonya-irsay <sanie@newschool.edu>
2019-06-04 18:52:04 -0400

    update google map + new video

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,4 @@
-// var coll = document.getElementsByClassName("collapsible");
-// var i;
-// //
-// for (i = 0; i < coll.length; i++) {
-//   coll[i].addEventListener("click", function() {
-//     // this.classList.toggle("active");
-//     // var content = this.nextElementSibling;
-//     var content = document.getElementById("menu");
-//     if (content.style.maxHeight){
-//       content.style.maxHeight = null;
-//       // document.getElementById("page").style.paddingTop = "5.6em";
-//     } else {
-//       content.style.maxHeight = content.scrollHeight + "px";
-//       // document.getElementById("page").style.paddingTop = content.scrollHeight / 18 + 8.1 + "em";
-//
-//       // console.log(content.scrollHeight / 18 + 6.1 + "em");
-//       // content.style.maxHeight = 100 + "%";
-//     }
-//   });
-// }
-
+// ------------------------------ Responsive Menu -----------------------------------------
 var x = document.getElementById("menu");
 
 function toggleMenu() {
@@ -41,3 +21,207 @@ function mediaQuery(y) {
 var y = window.matchMedia("(max-width: 950px)")
 mediaQuery(y) // Call listener function at run time
 y.addListener(mediaQuery) // Attach listener function on state changes
+
+
+// ------------------------------ Map -----------------------------------------
+var geocoder;
+var map;
+var address = "361 Stagg St Brooklyn NY 11206";
+function initMap() {
+  var styledMapType = new google.maps.StyledMapType(
+    [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#f5f5f5"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#bdbdbd"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#ffffff"
+          }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#757575"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#dadada"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#616161"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#e5e5e5"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#eeeeee"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#c9c9c9"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#9e9e9e"
+          }
+        ]
+      }
+    ],
+    {name: 'Styled Map'});
+
+  // Create a map object, and include the MapTypeId to add
+  // to the map type control.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: {lat: -34.397, lng: 150.644},
+    mapTypeControl: false,
+    fullscreenControl: false,
+    streetViewControl: false,
+    mapTypeControlOptions: {
+      mapTypeIds: ['styled_map']
+    }
+  });
+
+  //Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
+
+  geocoder = new google.maps.Geocoder();
+  codeAddress(geocoder, map);
+}
+
+function codeAddress(geocoder, map) {
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
